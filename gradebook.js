c7b7bd7354eb3aaf02eab8d7bf6e7ae556418a64
vanbruntj <jvanbrunt@my.waketech.edu>
2025-04-10 21:18:18 -0400

    GB up
    Gradebook

---
+++ gradebook.js
@@ -0,0 +1,16 @@
+// TODO: Fetch data from the PostgreSQL database (to be implemented later)
+function fetchGradeData() {
+	// This function will quesry the PostgreSQL database and return grade data
+	console.log("Fetching grade date...:);
+}
+
+// TODOL Populate the table with grade data
+function populateGradebook(data) {
+	// This function will take the fetched grade data and populate the table
+	console.log("Populating gradebook with data:", data);
+}
+
+// TODO REMOVE THIS
+// Call the stubs to demonstrate the workflow
+const gradeData = fetchGradeData();
+// END REMOVE
